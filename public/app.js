0187bfca9d7160ea26cb368f419a1efbafc8a87d
Ouzin <Ouzinvw@live.com>
2022-10-05 15:22:08 -0400

    Removed excess code

--- public/app.js
+++ public/app.js
@@ -3,14 +3,6 @@
 // 301229915
 // October 3, 2022
 
-(function () {
-    function Start() {
-        console.log('App started');
-    }
-
-    window.addEventListener('load', Start)
-})();
-
 // Submit and redirect to homepage
 document.getElementById("submitBtn").onclick = function(){
     location.href = "/";
